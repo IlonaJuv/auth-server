92095aabd8c97de262aad784639d0b64e4775b36
IlonaJuv <ilonaju@metropolia.fi>
2023-10-01 23:42:47 +0300

    first

--- src/api/routes/authRoute.ts
+++ src/api/routes/authRoute.ts
@@ -1,7 +1,11 @@
 import express from 'express';
 import {loginPost} from '../controllers/authController';
+import {body} from 'express-validator';
+
 const router = express.Router();
 
-router.post('/login', loginPost);
+router
+  .route('/login')
+  .post(body('username').escape(), body('password').escape(), loginPost);
 
 export default router;
